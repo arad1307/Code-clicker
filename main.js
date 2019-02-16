55919289f223953c619ec8c67ff1ee6f91585fac
Dara Cline <arad1307@gmail.com>
2019-02-16 17:57:20 -0600

    Add subcontractor feature

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 var linesOfCode = 0;
 var extraHands = 0;
 var codeMonkey = 0;
+var subContractor = 0;
 
 // getters
 
 const getExtraHandsCost = () => Math.round(10 * Math.pow(1.07, extraHands));
 
 const getCodeMonkeyCost = () => Math.round(100 * Math.pow(1.07, codeMonkey));
+
+const getSubContractorCost = () => Math.round(1000 * Math.pow(1.07, subContractor));
 
 // button actions
 
@@ -24,10 +27,19 @@ const onBuyExtraHands = () => {
 }
 
 const onBuyCodeMonkey = () => {
-    linesOfCode -= updateCodeMonkeyCost();
+    linesOfCode -= getCodeMonkeyCost();
     codeMonkey += 1;
     updateCodeMonkey();
     updateCount();
+    updateCodeMonkeyCost();
+}
+
+const onBuySubContractor = () => {
+    linesOfCode -= getSubContractorCost();
+    subContractor += 1;
+    updateSubContractor();
+    updateCount();
+    updateSubContractorCost();
 }
 
 // DOM updates
@@ -36,6 +48,7 @@ const updateCount = () => {
 	document.getElementById("lines-of-code").innerHTML = linesOfCode;
     document.getElementById("extra-hands-btn").disabled = linesOfCode < getExtraHandsCost();
     document.getElementById("code-monkey-btn").disabled = linesOfCode < getCodeMonkeyCost();
+    document.getElementById("sub-contractor-btn").disabled = linesOfCode < getSubContractorCost();
 }
 
 const updateHands = () => document.getElementById("extra-hands").innerHTML = extraHands;
@@ -47,10 +60,15 @@ const updateCodeMonkey = () => document.getElementById("code-monkey").innerHTML 
 
 const updateCodeMonkeyCost = () => document.getElementById("code-monkey-cost").innerHTML = getCodeMonkeyCost();
 
+const updateSubContractor = () => document.getElementById("sub-contractor").innerHTML = subContractor;
+
+const updateSubContractorCost = () => document.getElementById("sub-contractor-cost").innerHTML = getSubContractorCost();
+
 const updateCountPerSecond = () => {
-    linesOfCode += codeMonkey;
+    linesOfCode += codeMonkey + subContractor * 10;
     updateCount();
 }
+
 
 setInterval(updateCountPerSecond, 1000);
 
