20ff110a2fc4bbe07e811b851181218501319e61
Dara Cline <arad1307@gmail.com>
2019-02-16 17:36:42 -0600

    Add code monkey feature

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var linesOfCode = 0;
 var extraHands = 0;
-
+var codeMonkey = 0;
 
 const onButtonClick = () => {
 	linesOfCode += 1 + extraHands;
@@ -9,13 +9,14 @@ const onButtonClick = () => {
 
 const updateCount = () => {
 	document.getElementById("lines-of-code").innerHTML = linesOfCode;
-    document.getElementById("extra-hands-btn").disabled = linesOfCode < 10;
+    document.getElementById("extra-hands-btn").disabled = linesOfCode < getExtraHandsCost();
+    document.getElementById("code-monkey-btn").disabled = linesOfCode < getCodeMonkeyCost();
 }
 
 const onBuyExtraHands = () => {
+    linesOfCode -= getExtraHandsCost();
 	extraHands += 1;
 	updateHands();
-    linesOfCode -= 10;
     updateCount();
     updateExtraHandsCost();
 }
@@ -25,3 +26,25 @@ const updateHands = () => document.getElementById("extra-hands").innerHTML = ext
 const getExtraHandsCost = () => Math.round(10 * Math.pow(1.07, extraHands));
 
 const updateExtraHandsCost = () => document.getElementById("extra-hands-cost").innerHTML = getExtraHandsCost();
+
+const onBuyCodeMonkey = () => {
+    linesOfCode -= updateCodeMonkeyCost();
+    codeMonkey += 1;
+    updateCodeMonkey();
+    updateCount();
+}
+
+const getCodeMonkeyCost = () => Math.round(100 * Math.pow(1.07, codeMonkey));
+
+const updateCodeMonkey = () => document.getElementById("code-monkey").innerHTML = codeMonkey;
+
+const updateCodeMonkeyCost = () => document.getElementById("code-monkey-cost").innerHTML = getCodeMonkeyCost();
+
+const updateCountPerSecond = () => {
+    linesOfCode += codeMonkey;
+    updateCount();
+}
+
+setInterval(updateCountPerSecond, 1000);
+
+
