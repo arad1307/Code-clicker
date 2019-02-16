2c7929817d79079688fb260d0b54ef82f884f7ff
Dara Cline <arad1307@gmail.com>
2019-02-16 16:36:31 -0600

    Added extra hands feature

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 var linesOfCode = 0;
+var extraHands = 0;
+
 
 const onButtonClick = () => {
-	linesOfCode += 1;
+	linesOfCode += 1 + extraHands;
 	updateCount();
 }
 
-
 const updateCount = () => document.getElementById("lines-of-code").innerHTML = linesOfCode;
+
+const onBuyExtraHands = () => {
+	extraHands += 1;
+	updateHands();
+}
+
+const updateHands = () => document.getElementById("extra-hands").innerHTML = extraHands;
